a90c9ffd38dc6b2119a5f3d8b046cb9400b7cbbc
Madeline Savoiu <savoiu@csu.fullerton.edu>
2026-03-23 14:23:06 -0700

    configured database connection

---
+++ src/test/java/Madeline/Savoiu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Madeline.Savoiu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
